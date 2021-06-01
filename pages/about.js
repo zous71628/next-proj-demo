83ffb00c52ae17b887f2f7b0cff28ddcba570cd6
zous71628 <jack71628@gmail.com>
2021-06-01 19:11:02 +0800

    first commit

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react'
+esport default function about(){
+    return (
+        <dev>
+            <h1>About</h1>
+            <p>This is an app to find the latest DJ and other musical event</p>
+            <p>Version: 1.0.0</p>
+        </dev>
+    );
+}
